905a63acf963bf1803fdb7b7c30cf4f149d91549
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-06-22 00:38:35 +0300

    added seald-secrets controller

---
+++ deployment/terraform/helm.tf
@@ -0,0 +1,52 @@
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = var.ingress_nginx_version
+  namespace        = "ingress-nginx"
+  create_namespace = true        # ← Helm creates the namespace automatically
+
+  wait    = true
+  timeout = 300
+
+  values = [
+    file("${path.module}/values/ingress-nginx-values.yaml")
+  ]
+}
+
+
+
+# resource "helm_release" "argocd" {
+#   name             = "argocd"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argo-cd"
+#   namespace        = "argocd"
+#   create_namespace = true
+#   version          = "7.3.4"
+
+#   values = [
+#     templatefile("${path.module}/values/argocd-values.yaml", {
+#       ingress_host = var.argocd_ingress_host
+#     })
+#   ]
+
+#   depends_on = [
+#     module.eks,
+#     helm_release.ingress_nginx
+#   ]
+# }
+
+
+# resource "helm_release" "sealed_secrets" {
+#   name             = "sealed-secrets"
+#   repository       = "https://bitnami-labs.github.io/sealed-secrets"
+#   chart            = "sealed-secrets"
+#   namespace        = "kube-system"
+#   create_namespace = false
+#   version          = "2.15.3"
+
+#   depends_on = [module.eks]
+# }
+
+
+
