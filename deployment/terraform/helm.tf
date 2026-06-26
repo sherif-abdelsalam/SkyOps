468153c21dee422bf4e7bbbd2a32a666ce4a8937
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-06-26 18:38:06 +0300

    added helm chart for installing cert-manager

--- deployment/terraform/helm.tf
+++ deployment/terraform/helm.tf
@@ -4,7 +4,7 @@ resource "helm_release" "ingress_nginx" {
   chart            = "ingress-nginx"
   version          = var.ingress_nginx_version
   namespace        = "ingress-nginx"
-  create_namespace = true        # ← Helm creates the namespace automatically
+  create_namespace = true 
 
   wait    = true
   timeout = 300
@@ -37,68 +37,88 @@ resource "helm_release" "argocd" {
 }
 
 
+
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "oci://quay.io/jetstack/charts"
+  chart            = "cert-manager"
+  version          = "v1.20.2"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    },
+  ]
+    
+}
+
+
 # modules/argocd-image-updater/main.tf
 
-resource "helm_release" "argocd_image_updater" {
-  name             = "argocd-image-updater"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argocd-image-updater"
-  version          = "0.11.0"
-  namespace        = "argocd"
-  create_namespace = false  # argocd namespace already exists
+# resource "helm_release" "argocd_image_updater" {
+#   name             = "argocd-image-updater"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argocd-image-updater"
+#   version          = "0.11.0"
+#   namespace        = "argocd"
+#   create_namespace = false  # argocd namespace already exists
 
-  values = [
-    templatefile("${path.module}/values/argocd-image-updater.yaml", {
-      aws_region     = var.region
-      aws_account_id = var.aws_account_id
-    })
-  ]
-}
+#   values = [
+#     templatefile("${path.module}/values/argocd-image-updater.yaml", {
+#       aws_region     = var.region
+#       aws_account_id = var.aws_account_id
+#     })
+#   ]
+# }
 
 
 
-data "aws_iam_policy_document" "image_updater_assume" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "image_updater_assume" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect  = "Allow"
 
-    principals {
-      type        = "Federated"
-      identifiers = [module.eks.oidc_provider_arn]
-    }
+#     principals {
+#       type        = "Federated"
+#       identifiers = [module.eks.oidc_provider_arn]
+#     }
 
-    condition {
-      test     = "StringEquals"
-      variable = "${module.eks.oidc_provider}:sub"
-      values   = ["system:serviceaccount:argocd:argocd-image-updater"]
-    }
-  }
-} 
+#     condition {
+#       test     = "StringEquals"
+#       variable = "${module.eks.oidc_provider}:sub"
+#       values   = ["system:serviceaccount:argocd:argocd-image-updater"]
+#     }
+#   }
+# } 
 
-resource "aws_iam_role" "image_updater" {
-  name               = "argocd-image-updater"
-  assume_role_policy = data.aws_iam_policy_document.image_updater_assume.json
-}
+# resource "aws_iam_role" "image_updater" {
+#   name               = "argocd-image-updater"
+#   assume_role_policy = data.aws_iam_policy_document.image_updater_assume.json
+# }
 
-resource "aws_iam_role_policy" "image_updater_ecr" {
-  name = "ecr-read"
-  role = aws_iam_role.image_updater.id
+# resource "aws_iam_role_policy" "image_updater_ecr" {
+#   name = "ecr-read"
+#   role = aws_iam_role.image_updater.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:DescribeImages",
-          "ecr:DescribeRepositories",
-          "ecr:ListImages"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ecr:GetAuthorizationToken",
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:DescribeImages",
+#           "ecr:DescribeRepositories",
+#           "ecr:ListImages"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
